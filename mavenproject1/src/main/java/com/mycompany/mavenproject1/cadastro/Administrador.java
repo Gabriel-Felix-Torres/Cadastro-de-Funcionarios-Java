84719174c1f74ed1dfdb68b7dfdba0c33a693b40
Gabriel-Felix-Torres <167694614+Gabriel-Felix-Torres@users.noreply.github.com>
2024-12-01 11:28:14 -0300

    add projeto

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/cadastro/Administrador.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1.cadastro;
+
+/**
+ *
+ * @author gabri
+ */
+public class Administrador {
+    private String usuario;
+    private String senha;
+    
+    public Administrador(String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+            
+}
